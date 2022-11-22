aea9aa01120d5ff3775be58980c9ada01c7757f9
hostem17 <vjjais17@gmail.com>
2022-11-22 23:30:00 +0530

    gift tokens done

--- app.js
+++ app.js
@@ -22,35 +22,7 @@ const contract = new web3.eth.Contract(assets.abi, assets.address);
 
 async function readBalance(address) {
     const balance = await contract.methods.balanceOf(address).call();
-    console.log(balance.toString());
     return balance.toString();
-
-}
-
-async function transfer(address, amount) {
-    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
-    const gasEstimate = await contract.methods.sendTokens(address, amount).estimateGas({ 'from': PUBLIC_KEY }); // estimate gas
-    // Create the transaction
-    const tx = {
-        'from': PUBLIC_KEY,
-        'to': assets.address,
-        'nonce': nonce,
-        'gas': gasEstimate,
-        'data': contract.methods.sendTokens(address, amount).encodeABI()
-    };
-
-    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
-    signPromise.then((signedTx) => {
-        web3.eth.sendSignedTransaction(signedTx.rawTransaction, function (err, hash) {
-            if (!err) {
-                console.log("The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
-            } else {
-                console.log("Something went wrong when submitting your transaction:", err)
-            }
-        });
-    }).catch((err) => {
-        console.log("Promise failed:", err);
-    });
 }
 
 // To check Address's validity
@@ -82,7 +54,6 @@ app.get('/:address', async (req, res) => {
             error: null
         });
     } catch (e) {
-        console.log(e.code);
         res.status(400).send({
             valid: false,
             balance: 0,
@@ -99,20 +70,60 @@ app.post('/giftTokens', async (req, res) => {
     var keySent = req.body.key;
 
     if (keySent != Key) {
-        console.log(keySent);
-        console.log(Key);
+        res.status(401).send(
+            {
+                error: "Unauthorised",
+                amount: 0,
+            }
+        );
+    }
+    else if (!(web3.utils.isAddress(address))) {
+        res.status(404).send({
+            amount: 0,
+            error: "Invalid Address"
+        });
+    } else if (parseFloat(amount) < 0) {
+        res.status(404).send({
+            amount: 0,
+            error: "Invalid Amount"
+        });
+    }
+    else {
 
-        res.sendStatus(401).send("Unauthorised");
-    } else {
         var amountInWei = BigNumber(amount).multiply(1000000000000000000).toString();
 
-        await transfer(address, amountInWei);
+        const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+        const gasEstimate = await contract.methods.sendTokens(address, amount).estimateGas({ 'from': PUBLIC_KEY }); // estimate gas
 
-        res.sendStatus(200).send(amount);
+        const tx = {
+            'from': PUBLIC_KEY,
+            'to': assets.address,
+            'nonce': nonce,
+            'gas': gasEstimate,
+            'data': contract.methods.sendTokens(address, amountInWei).encodeABI()
+        };
+
+        const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+
+        signPromise.then(async (signedTx) => {
+            await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+            res.status(200).send({
+                amount: amount,
+                error: null,
+            });
+
+
+        }).catch((err) => {
+            res.status(503).send({
+                amount: amount,
+                error: "insufficient funds for gas"
+            });
+
+        });
     }
-
-
 })
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`port${port} `));
